b4e2facaa67a099085e0070e36543ac07f62c395
qiu005 <157332731+qiu005@users.noreply.github.com>
2026-01-18 20:42:45 -0500

    Replace printf with perror for error handling

--- server.c
+++ server.c
@@ -19,7 +19,7 @@ int main(int argc, char*argv[]){
    
    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
    if(sockfd==-1){
-       printf("socket initialization fails\n");
+       perror("socket initialization fails\n");
    }
     
     struct sockaddr_in addr;
@@ -30,7 +30,7 @@ int main(int argc, char*argv[]){
     memset(addr.sin_zero,'\0', sizeof(addr.sin_zero)); //from Beej's Guide to Network Programming page 25
         
     if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr))==-1){
-        printf("bind fails\n");
+        perror("bind fails\n");
         return 1;
     }
    
@@ -39,20 +39,20 @@ int main(int argc, char*argv[]){
     socklen_t src_addrlen = sizeof(src_addr);
     ssize_t receive = recvfrom (sockfd, buf, sizeof(buf), 0, (struct sockaddr*)&src_addr, &src_addrlen);
     if(receive==-1){
-        printf("receive error\n");
+        perror("receive error\n");
         return 1;
     }
 
     if(strcmp(buf,"ftp")==0){
         ssize_t send1 = sendto (sockfd, "yes", strlen("yes"), 0, (struct sockaddr*)&src_addr, src_addrlen);
         if(send1==-1){
-            printf("send error\n");
+            perror("send error\n");
             return 1;
         }
     }else{
         ssize_t send1 = sendto (sockfd, "no", strlen("no"), 0, (struct sockaddr*)&src_addr, src_addrlen);
         if(send1==-1){
-            printf("send error\n");
+            perror("send error\n");
             return 1;
         }
     }
